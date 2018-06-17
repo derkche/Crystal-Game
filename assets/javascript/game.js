f54b92038af674e292bb88a7fd34db6c5e607973
derkche <derekchee.40sw@gmail.com>
2018-06-17 11:39:14 -0400

    Added some styling and some js bits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var losses = 0;
    
     //sets the winning number
-    var gemsToDeposit = [Math.floor(Math.random()*(120-19+1))+19]
+    var gemsToDeposit = (Math.floor(Math.random()*(120-19+1))+19)
     $("#gems_to_deposit").html(gemsToDeposit);
 
     var gemsDeposited = 0;
@@ -16,79 +16,183 @@ $(document).ready(function() {
     var goldValue = (Math.floor(Math.random()*(12-1+1))+1)
     
     $("#GreenRupee").on("click", function() {
+
         gemsDeposited += greenValue;
         $("#userGemCount").html(gemsDeposited);
+
         if (gemsDeposited == gemsToDeposit) {
+
             alert("You got it!");
             wins ++
             $("#wins").html(wins);
+
+            gemsToDeposit = (Math.floor(Math.random()*(120-19+1))+19)
+            $("#gems_to_deposit").html(gemsToDeposit);
+
+            gemsDeposited = 0;
+            $("#userGemCount").html(gemsDeposited);
+
+            greenValue = (Math.floor(Math.random()*(12-1+1))+1);
+            blueValue = (Math.floor(Math.random()*(12-1+1))+1);
+            redValue = (Math.floor(Math.random()*(12-1+1))+1);
+            goldValue = (Math.floor(Math.random()*(12-1+1))+1);
+
         };
-    
+
         if (gemsDeposited >= gemsToDeposit) {
+
             alert("Oops you went over");
             losses ++
             $("#losses").html(losses);
+
+            gemsToDeposit = (Math.floor(Math.random()*(120-19+1))+19);
+            $("#gems_to_deposit").html(gemsToDeposit);
+
+            gemsDeposited = 0;
+            $("#userGemCount").html(gemsDeposited);
+
+            greenValue = (Math.floor(Math.random()*(12-1+1))+1);
+            blueValue = (Math.floor(Math.random()*(12-1+1))+1);
+            redValue = (Math.floor(Math.random()*(12-1+1))+1);
+            goldValue = (Math.floor(Math.random()*(12-1+1))+1);
+
         };
+
     });
 
     $("#BlueRupee").on("click", function() {
+
         gemsDeposited += blueValue;
         $("#userGemCount").html(gemsDeposited);
+
         if (gemsDeposited == gemsToDeposit) {
+
             alert("You got it!");
             wins ++
             $("#wins").html(wins);
+
+            gemsToDeposit = (Math.floor(Math.random()*(120-19+1))+19)
+            $("#gems_to_deposit").html(gemsToDeposit);
+
+            gemsDeposited = 0;
+            $("#userGemCount").html(gemsDeposited);
+
+            greenValue = (Math.floor(Math.random()*(12-1+1))+1);
+            blueValue = (Math.floor(Math.random()*(12-1+1))+1);
+            redValue = (Math.floor(Math.random()*(12-1+1))+1);
+            goldValue = (Math.floor(Math.random()*(12-1+1))+1);
+
         };
-    
+
         if (gemsDeposited >= gemsToDeposit) {
+
             alert("Oops you went over");
             losses ++
             $("#losses").html(losses);
+
+            gemsToDeposit = (Math.floor(Math.random()*(120-19+1))+19);
+            $("#gems_to_deposit").html(gemsToDeposit);
+
+            gemsDeposited = 0;
+            $("#userGemCount").html(gemsDeposited);
+
+            greenValue = (Math.floor(Math.random()*(12-1+1))+1);
+            blueValue = (Math.floor(Math.random()*(12-1+1))+1);
+            redValue = (Math.floor(Math.random()*(12-1+1))+1);
+            goldValue = (Math.floor(Math.random()*(12-1+1))+1);
+
         };
+
     });
 
     $("#RedRupee").on("click", function() {
+
         gemsDeposited += redValue;
         $("#userGemCount").html(gemsDeposited);
+
         if (gemsDeposited == gemsToDeposit) {
+
             alert("You got it!");
             wins ++
             $("#wins").html(wins);
+
+            gemsToDeposit = (Math.floor(Math.random()*(120-19+1))+19)
+            $("#gems_to_deposit").html(gemsToDeposit);
+
+            gemsDeposited = 0;
+            $("#userGemCount").html(gemsDeposited);
+
+            greenValue = (Math.floor(Math.random()*(12-1+1))+1);
+            blueValue = (Math.floor(Math.random()*(12-1+1))+1);
+            redValue = (Math.floor(Math.random()*(12-1+1))+1);
+            goldValue = (Math.floor(Math.random()*(12-1+1))+1);
+
         };
-    
+
         if (gemsDeposited >= gemsToDeposit) {
+
             alert("Oops you went over");
             losses ++
             $("#losses").html(losses);
+
+            gemsToDeposit = (Math.floor(Math.random()*(120-19+1))+19);
+            $("#gems_to_deposit").html(gemsToDeposit);
+
+            gemsDeposited = 0;
+            $("#userGemCount").html(gemsDeposited);
+
+            greenValue = (Math.floor(Math.random()*(12-1+1))+1);
+            blueValue = (Math.floor(Math.random()*(12-1+1))+1);
+            redValue = (Math.floor(Math.random()*(12-1+1))+1);
+            goldValue = (Math.floor(Math.random()*(12-1+1))+1);
+
         };
+
     });
 
     $("#GoldRupee").on("click", function() {
-        gemsDeposited += goldValue
+
+        gemsDeposited += goldValue;
         $("#userGemCount").html(gemsDeposited);
+
         if (gemsDeposited == gemsToDeposit) {
+
             alert("You got it!");
             wins ++
             $("#wins").html(wins);
+
+            gemsToDeposit = (Math.floor(Math.random()*(120-19+1))+19)
+            $("#gems_to_deposit").html(gemsToDeposit);
+
+            gemsDeposited = 0;
+            $("#userGemCount").html(gemsDeposited);
+
+            greenValue = (Math.floor(Math.random()*(12-1+1))+1);
+            blueValue = (Math.floor(Math.random()*(12-1+1))+1);
+            redValue = (Math.floor(Math.random()*(12-1+1))+1);
+            goldValue = (Math.floor(Math.random()*(12-1+1))+1);
+
         };
-    
+
         if (gemsDeposited >= gemsToDeposit) {
+
             alert("Oops you went over");
             losses ++
             $("#losses").html(losses);
-        };
-    });
-    
-    if (gemsDeposited == gemsToDeposit) {
-        alert("You got it!");
-        wins ++
-        $("#wins").html(wins);
-    };
 
-    if (gemsDeposited >= gemsToDeposit) {
-        alert("Oops you went over");
-        losses ++
-        $("#losses").html(losses);
-    };
+            gemsToDeposit = (Math.floor(Math.random()*(120-19+1))+19);
+            $("#gems_to_deposit").html(gemsToDeposit);
+
+            gemsDeposited = 0;
+            $("#userGemCount").html(gemsDeposited);
+
+            greenValue = (Math.floor(Math.random()*(12-1+1))+1);
+            blueValue = (Math.floor(Math.random()*(12-1+1))+1);
+            redValue = (Math.floor(Math.random()*(12-1+1))+1);
+            goldValue = (Math.floor(Math.random()*(12-1+1))+1);
+
+        };
+
+    });
 
 });
